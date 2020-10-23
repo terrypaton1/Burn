2b349495ec528fb2f9161292f0d8a77073ff9525
Terry Paton <terrypaton1@gmail.com>
2020-10-23 21:19:37 +1100

    Add missing materials for thanks scene
    Randomize the rotation of the thanks models on startup.
    Fix up placement of Seth’s button.

--- Burn/Assets/Scenes/UI/Scripts/Thanks/RotateThanksModels.cs
+++ Burn/Assets/Scenes/UI/Scripts/Thanks/RotateThanksModels.cs
@@ -5,6 +5,12 @@ public class RotateThanksModels : MonoBehaviour
     [SerializeField]
     public int speed = 1;
 
+    protected void OnEnable()
+    {
+        var rotation = Vector3.up * Random.Range(-180.0f, 180.0f);
+        transform.Rotate(rotation);
+    }
+
     public void Update()
     {
         var rotation = Vector3.up * (Time.deltaTime * speed);
